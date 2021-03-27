fc932070508e32e0bdc1a3a0162ed52d7020672c
youngfisher <yuyang0224@gmail.com>
2021-03-27 14:56:55 +0800

    Update BuildMaxHeap.h

--- Introduction to algorighms/BuildMaxHeap.h
+++ Introduction to algorighms/BuildMaxHeap.h
@@ -2,5 +2,8 @@
 #define BUILDMAXHEAP_H
 
 void BuildMaxHeap(vector<int> &A);
+//i is the index for counting vector A, ranges between 1 and A.size().heapSize <= A.size()
+void max_Heapify(vector<int> &A, int i, int heapSize);
+
 
 #endif

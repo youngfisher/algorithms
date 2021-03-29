28e52788d709ebbeae8e5ab195f36c511585b230
youngfisher <yuyang0224@gmail.com>
2021-03-29 22:12:45 +0800

    Update HeapSort.cpp

--- Introduction to algorighms/HeapSort.cpp
+++ Introduction to algorighms/HeapSort.cpp
@@ -1,17 +1,29 @@
 #include <vector>
-#include "HeapSort.h"
-#include "BuildMaxHeap.h"
+#include "Heap.h"
 
 using namespace std;
 
-void HeapSort(vector<int> &A){
-    BuildMaxHeap(A);
-    int len = A.size();
+void HeapSort(vector<int> &A, int flag){//flag==1, ascend order. flag==-1,descend order.
 
-    for(int i=len,heapSize=len;i>=2;i--){//index for counting lies between 1 and len
-        swap(A[i-1],A[0]);
-        heapSize--;
-        max_Heapify(A,1,heapSize);//index 1 corresponds to A[0]
+    if(flag==1){
+        BuildMaxHeap(A);
+        int len = A.size();
+
+        for(int i=len,heapSize=len;i>=2;i--){//index for counting lies between 1 and len
+            swap(A[i-1],A[0]);
+            heapSize--;
+            max_Heapify(A,1,heapSize);//index 1 corresponds to A[0]
+        }
+    }
+    else if(flag==-1){
+        BuildMinHeap(A);
+        int len = A.size();
+
+        for(int i=len,heapSize=len;i>=2;i--){//index for counting lies between 1 and len
+            swap(A[i-1],A[0]);
+            heapSize--;
+            min_Heapify(A,1,heapSize);//index 1 corresponds to A[0]
+        }        
     }
 
 }

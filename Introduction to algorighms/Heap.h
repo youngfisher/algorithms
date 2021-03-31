b6a3fc82822f9cc9a664f386c44c754dc1926d1b
youngfisher <yuyang0224@gmail.com>
2021-03-31 12:31:30 +0800

    update PriorityQueue declaration code
    employ surrogate class technique

--- Introduction to algorighms/Heap.h
+++ Introduction to algorighms/Heap.h
@@ -2,6 +2,7 @@
 #define HEAP_H
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -13,6 +14,35 @@ void BuildMinHeap(vector<int> &A);
 //i is the index for counting vector A, ranges between 1 and A.size().heapSize <= A.size()
 void min_Heapify(vector<int> &A, int i, int heapSize);
 
-void HeapSort(vector<int> &A, int flag);//sorting from small to big
+void HeapSort(vector<int> &A, int flag);//sorting from small to big if flag == 1. flag == -1 means the opposite
+
+class MaxPriorityQueue;
+class MinPriorityQueue;
+
+class PriorityQueue{
+    public:
+        PriorityQueue(const vector<int>& A, string queueType);//queueType == "max" or "min"
+        PriorityQueue(vector<int>&& A, string queueType);
+        ~PriorityQueue();
+          
+        //shared functions
+        void PrintQueue() const;
+        void Insert(int key);
+        
+        //only apply to MinPriorityQueue
+        void DecreaseKey(int i, int key);
+        int ExtractMinimum();
+        int Minimum() const;
+
+        //only apply to MaxPriorityQueue
+        void IncreaseKey(int i, int key);
+        int ExtractMaximum();
+        int Maximum() const;        
+
+    private:
+        MaxPriorityQueue* maxQueue{nullptr};
+        MinPriorityQueue* minQueue{nullptr};
+
+};
 
 #endif

4be4980f441fe934ed689891e6416144026a84a9
youngfisher <yuyang0224@gmail.com>
2021-04-22 16:15:14 +0800

    Create chapter6_5_6_Queue.cpp

---
+++ Introduction to algorighms/chapter6_5_6_Queue.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+#include "Queue.h"
+
+
+using namespace std;
+/*
+int main(){
+
+    vector<double> A={1.3,5.4,6.7,9.8,-2.6};
+    Queue<double> test(A);
+
+    test.PrintQueue();
+
+    test.Dequeue();
+
+    test.PrintQueue();
+
+    test.Enqueue(2,100);
+
+    test.PrintQueue();
+
+    cout << test.GetPriority(3);
+
+    return 0;
+
+}
+
+*/

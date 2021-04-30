d64dadf043847736f90fd693325ef6da46b87b66
youngfisher <yuyang0224@gmail.com>
2021-04-30 12:45:59 +0800

    Create list.cpp

---
+++ Introduction to algorighms/list.cpp
@@ -0,0 +1,21 @@
+#include "list.h"
+
+/*
+int main(){
+    list<int> A;
+    vector<int> test = {1,2,3,4,50};
+    list<int> B(test);
+    list<int> C(B);
+    list<int> D = C;
+    D.insert(100);
+    D.deletion(D.get_header()->next);
+
+    //test of p232, introduction to algorithms
+    unsigned int t = 2654435769;
+    int t1 = t>>18;
+
+    cout<<t1<<endl;
+
+    return 0;
+}
+*/
